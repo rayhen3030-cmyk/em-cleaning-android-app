fa320f5353d6db6c711847213f873388342a9c59
rayhen3030-cmyk <rayhen3030@gmail.com>
2026-08-10 10:22:40 +0200

    Implement file chooser in WebView for image uploads

--- app/src/main/java/de/emcleaning/app/MainActivity.java
+++ app/src/main/java/de/emcleaning/app/MainActivity.java
@@ -1,25 +1,124 @@
 package de.emcleaning.app;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
+    private WebView webView;
+
+    private ValueCallback<Uri[]> filePathCallback;
+
+    private static final int FILE_CHOOSER_REQUEST = 1001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WebView webView = new WebView(this);
+        webView = new WebView(this);
         setContentView(webView);
 
         WebSettings settings = webView.getSettings();
+
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
 
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("file:///android_asset/index.html");
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public boolean onShowFileChooser(
+                    WebView webView,
+                    ValueCallback<Uri[]> filePathCallbackNew,
+                    FileChooserParams fileChooserParams) {
+
+                if (filePathCallback != null) {
+                    filePathCallback.onReceiveValue(null);
+                }
+
+                filePathCallback = filePathCallbackNew;
+
+                Intent intent;
+
+                try {
+                    intent = fileChooserParams.createIntent();
+                    intent.setType("image/*");
+
+                    startActivityForResult(
+                            intent,
+                            FILE_CHOOSER_REQUEST
+                    );
+
+                } catch (Exception e) {
+
+                    filePathCallback = null;
+                    return false;
+                }
+
+                return true;
+            }
+        });
+
+        webView.loadUrl(
+                "file:///android_asset/index.html"
+        );
+    }
+
+
+    @Override
+    protected void onActivityResult(
+            int requestCode,
+            int resultCode,
+            Intent data) {
+
+        super.onActivityResult(
+                requestCode,
+                resultCode,
+                data
+        );
+
+        if (requestCode == FILE_CHOOSER_REQUEST) {
+
+            if (filePathCallback == null) {
+                return;
+            }
+
+            Uri[] results = null;
+
+            if (resultCode == RESULT_OK) {
+
+                results =
+                        WebChromeClient.FileChooserParams
+                                .parseResult(
+                                        resultCode,
+                                        data
+                                );
+            }
+
+            filePathCallback.onReceiveValue(results);
+
+            filePathCallback = null;
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
